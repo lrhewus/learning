3445dd4cb9c71244bd7a711437b3d74a6755c56c
Lauren Hewus <Blinky@Laurens-MacBook-Pro.local>
2015-06-03 14:07:33 -0700

    inital commit

---
+++ ex3.py
@@ -0,0 +1,4 @@
+print 3 + 4
+print 5 - 2
+print 3.0 / 6.0
+print 3.0 / 6.0 + 5
---
+++ ex5.py
@@ -0,0 +1,16 @@
+my_name = 'Lauren R. Hewus'
+my_age = 26 
+my_height = 69 
+my_weight = 100
+my_eyes = 'Shit Brown'
+my_teeth = 'Perfect'
+my_hair = 'Too short'
+
+print "Let's talk about %s." % my_name
+print "She's %d inches tall." % my_height
+print "She's %d pounds of good lookin." % my_weight
+print "Actually that's not too shabby!"
+print "She's got %s eyes and %s of hair." % (my_eyes , my_hair) 
+print "Her teeth are always %s depending on the meth." % my_teeth
+
+print "If I add %d, %d, and %d I get %d." %  (my_age, my_height, my_weight, my_age + my_height + my_weight) 
