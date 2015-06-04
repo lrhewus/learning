89a69398cf5f8be415cb98fea6768fce74d5ebf7
Lauren Hewus <lrnhws@gmail.com>
2015-06-04 14:48:27 -0700

    exercise 13 complete

---
+++ ex13.py
@@ -0,0 +1,15 @@
+from sys import argv
+
+
+script, first, second, third = argv
+
+print "The script is called:" , script
+print "Your first variable is:" , first
+print "Your second variable is:" , second
+print "Your third variable is:" , third
+
+one = raw_input("What is the first argument? ")
+two = raw_input("What is the second argument? ")
+
+print one
+print two
