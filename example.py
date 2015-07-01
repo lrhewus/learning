edf6166e7479828f241d27e38334385f4cd16e5b
Lauren Hewus <lrnhws@gmail.com>
2015-07-01 12:37:56 -0700

    fixed!

--- example.py
+++ example.py
@@ -16,20 +16,20 @@ class Dog(object):
     def bark(self):
         print "%s is barking!" % self.name
 
-    def sniff(self):
-        print "%s is sniffing %s" % (dog1.name, dog2.name)
+    def sniff(self, other):
+        print "%s is sniffing %s" % (self.name, other.name)
 
-    def eat_food(self):
-        print "%s is eating %s's food" % (dog2.name, dog1.name)
+    def eat_food(self, other):
+        print "%s is eating %s's food" % (self.name, other.name)
 
-    def growl(self):
-        print "%s growls at %s" % (dog1.name, dog2.name)
+    def growl(self, other):
+        print "%s growls at %s" % (self.name, other.name)
 
-    def snap(self):
-        print "%s snaps at %s and stands her ground!" % (dog1.name, dog2.name)
+    def snap(self, other):
+        print "%s snaps at %s and stands her ground!" % (self.name, other.name)
 
     def run(self):
-        print "%s run's away!" % dog2.name
+        print "%s run's away!" % self.name
 
 dog1 = Dog('Lily', 5, 'brown', 'pitbull')
 dog2 = Dog('Comet', 6, 'white', 'lab')
@@ -42,9 +42,10 @@ dog2.walk()
 dog1.wag_tail()
 dog2.bark()
 dog1.bark()
-dog1.sniff()
+dog1.sniff(dog2)
 dog2.wag_tail()
-dog2.eat_food()
-dog1.growl()
-dog1.snap()
+dog2.sniff(dog1)
+dog2.eat_food(dog1)
+dog1.growl(dog2)
+dog1.snap(dog2)
 dog2.run()
